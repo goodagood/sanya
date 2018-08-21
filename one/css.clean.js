c2dab17a41d0e5276b144ebe66541fd87d8999e0
andrew <bin1510@gmail.com>
2018-08-21 12:17:14 +0800

    2018 0821 1217pm

--- one/css.clean.js
+++ one/css.clean.js
@@ -11,62 +11,41 @@ var options = { /* options */ };
 //var output = new CleanCSS(options).minify(input);
 //console.log(output);
 
+
+
+var outputFile = "/tmp/a.css";
 //var cssInput = "./public/out.css";
-var cssInput = "./public/src/a.css";
+var cssFile = "./public/src/a.css";
 
-//fs.readFile(cssInput, 'utf8', (err, data)=>{
-//    if(err) return console.log('err: ', err);
-//
-//    console.log(data);
-//    //console.log(data.toString());
-//    var mini = new CleanCSS({
-//        inline:['all'],
-//        format:'beautify'
-//    }).minify(data, (err, minified)=>{
-//
-//
-//        p('write');
-//        p(minified);
-//        fs.writeFile('/tmp/a.css', minified, 'utf8', (err)=>{
-//            if(err) console.log('write /tmp/a.css err', err);
-//        });
-//
-//    });
-//
-//});
+var cssFileHash = {
+    './public/src/normalize.css': {
+        'styles': fs.readFileSync('./public/src/normalize.css', 'utf8')},
+    './node_modules/animate.css/animate.css': {
+        'styles': fs.readFileSync('./node_modules/animate.css/animate.css', 'utf8')},
+    './public/src/style/test.css':        {'styles': fs.readFileSync('./public/src/style/test.css', 'utf8')}
+};
 
 
 
+// give the Hash of css files, .minify() will concatenate them to single output.
+// 0820
 
-fs.readFile(cssInput, 'utf8', (err, data)=>{
-    if(err) return console.log('err: ', err);
+var m = new CleanCSS({
+    inline:['all'],
+    format:'beautify'
+}).minify(cssFileHash, (err, minified)=>{
+    if(err){return p('.minify err: ', err);}
 
-    console.log(data);
-    //console.log(data.toString());
-    var mini = new CleanCSS({
-        inline:['all'],
-        format:'beautify'
-    }).minify({
-            './public/src/normalize.css': {'styles': fs.readFileSync('./public/src/normalize.css', 'utf8')},
-            './public/src/a.scss':        {'styles': fs.readFileSync('./public/src/a.scss', 'utf8')}
-        }, (err, minified)=>{
-
-
-            p('write');
-            p(minified);
-            fs.writeFile('/tmp/a.css', minified.styles, 'utf8', (err)=>{
-                if(err) console.log('write /tmp/a.css err', err);
-            });
-
-        });
+    p('before write');
+    //p(minified);
+    fs.writeFile(outputFile, minified.styles, 'utf8', (err)=>{
+        if(err) console.log('write err', err);
+    });
 
 });
 
 
-var ta = {
-    './public/src/normalize.css': {'styles': fs.readFileSync('./public/src/normalize.css', 'utf8')},
-    './public/src/a.scss':        {'styles': fs.readFileSync('./public/src/a.scss', 'utf8')}
-}
+
 
 
 
